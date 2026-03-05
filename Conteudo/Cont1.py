39e9f4459840008da7084ce02bd2de1565d9bbe3
Serebay <joao.serebay@escola.pr.gov.br>
2026-03-05 11:35:39 -0300

    Add comments explaining variable types in Python

---
+++ Conteudo/Cont1.py
@@ -0,0 +1,5 @@
+    #int para números inteiros. Ex: 7, 0,-4.
+    #float para números reais ou com virgula. Ex: 0.5, 7.6.
+    #bool para valores de verdadeiro ou falso. Ex: True ou False (Sempre usar com letra maiúscula)
+    #Str para strings que são palavras caracteres ou textos. Ex: 'ola', '7.5'.
+    #Para saber o tipo de uma variável pode ser usar o comando 'print(type(variavel))'
