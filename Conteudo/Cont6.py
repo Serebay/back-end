76c1e3eea3e58fad72e497ad3f8d778406fe9d3d
Serebay <joao.serebay@escola.pr.gov.br>
2026-03-16 10:51:39 -0300

    Create Cont6.py

---
+++ Conteudo/Cont6.py
@@ -0,0 +1,7 @@
+#OPERADORES
+#Podemos escrever um resultado na quantidade de caracteres específica ao colocar o valor dentro da mácara após escrever
+nome = input ('Qual é o seu nome?')
+print('prazer em conhece-lo {:^20}!'.format(nome))
+print('prazer em conhece-lo {:>20}!'.format(nome))
+print('prazer em conhece-lo {:<20}'.format(nome))
+print('prazer em conhece-lo {:^20}!'.format(nome))
