2e909a639fd0b3cd664b27926eae1e6d85ba8da5
Serebay <joao.serebay@escola.pr.gov.br>
2026-03-16 10:57:17 -0300

    Add exe006.py to analyze integer predecessor and successor

---
+++ Exercicíos/exe006.py
@@ -0,0 +1,6 @@
+numero = int(input("Digite um número inteiro: "))
+antecessor = numero - 1
+sucessor = numero + 1
+print(f"Analisando o valor {numero}:")
+print(f"O seu antecessor é {antecessor}")
+print(f"O seu sucessor é {sucessor}")
