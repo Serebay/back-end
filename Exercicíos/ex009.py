d38f9e520d20ccebf75940f463dc9e388ce8e607
Serebay <joao.serebay@escola.pr.gov.br>
2026-03-19 08:26:49 -0300

    Create ex009.py

---
+++ Exercicíos/ex009.py
@@ -0,0 +1,4 @@
+metros = float(input("Quantos metros?"))
+centimetros = metros*100
+milimetros = metros*1000
+print(f"{metros} é equivalente a {centimetros} centimetros e a {milimetros} milimetros.")
