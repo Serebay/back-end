55e87d139f1f1582f873e9614fb0e40da6a9aa37
Serebay <joao.serebay@escola.pr.gov.br>
2026-03-19 07:52:29 -0300

    Create ex006.py

---
+++ Exercicíos/ex006.py
@@ -0,0 +1,5 @@
+numero = int(input("Digite um número inteiro: "))
+antecessor = numero - 1
+sucessor = numero + 1
+print(f"Analisando o valor {numero}...")
+print(f"O seu antecessor é {antecessor} e o seu sucessor é {sucessor}.")
