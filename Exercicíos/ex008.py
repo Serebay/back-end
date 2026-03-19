4f3a383756a79599d04aa2e44523d1902d0be342
Serebay <joao.serebay@escola.pr.gov.br>
2026-03-19 08:14:10 -0300

    Create ex008.py

---
+++ Exercicíos/ex008.py
@@ -0,0 +1,4 @@
+nota1 = float(input("Digite a primeira nota:"))
+nota2 = float(input("Digite a segunda nota:"))
+media = (nota1 + nota2)/2
+print(f"Sua primeira nota:{nota1}, sua segunda nota:{nota2}, a média é: {media}")
