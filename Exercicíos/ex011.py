80ada07a0e46e5a6608c9d926257dc0f581ad9a3
Serebay <joao.serebay@escola.pr.gov.br>
2026-03-19 08:52:16 -0300

    Create ex011.py

---
+++ Exercicíos/ex011.py
@@ -0,0 +1,3 @@
+dindin = float(input("Digite a quantia de dinheiro desejada:"))
+dolar = dindin/5.24
+print("Esta quantia de dinheiro em reais: {:.0f} é equivalente a {:.0f} em dólares.".format(dindin, dolar))
