d66d161d59050c373e3034c7d29315535c73a71f
Serebay <joao.serebay@escola.pr.gov.br>
2026-03-19 08:01:04 -0300

    Create ex007.py

---
+++ Exercicíos/ex007.py
@@ -0,0 +1,5 @@
+numero = int(input("Digite um número inteiro: "))
+dobro = numero*2
+triplo = numero*3
+raiz = numero**0.5
+print(f"O número digitado foi:{numero}, o dobro é: {dobro}, o triplo é:{triplo} e a raiz é:{raiz}")
