dd8f80ed2a8d56f225924f194ca064b02c51a38d
Serebay <joao.serebay@escola.pr.gov.br>
2026-03-26 07:50:41 -0300

    Update ex014.py

--- Exercicíos/ex014.py
+++ Exercicíos/ex014.py
@@ -1,3 +1,3 @@
 SalarioAtual = float (input("Digite seu salário:"))
-SalarioNovo = SalarioAtual*1.15
+SalarioNovo = (SalarioAtual*15/100)
 print("Seu salário era: {:.2f}, o novo salário será: {:.2f}".format(SalarioAtual, SalarioNovo))
