26d9048799a928e45b12ae1668ff64e5627fe1d4
Serebay <joao.serebay@escola.pr.gov.br>
2026-03-26 08:23:20 -0300

    Create Cont07.py

---
+++ Conteudo/Cont07.py
@@ -0,0 +1,6 @@
+#MODULOS EM PYTHON
+# - Para inportar bibliotecas e funcionalidades em python utiliza-se o comando inport
+# - seguido do nome da biblioteca ou modulo que ira ser carregado neste programa.
+
+# - Para importar coisas especificas de uma biblioteca utiliza-se o comando from
+# - seguido da palavra inport seguido do item q deseja inportar.
